9ff76f438e8a7f27e008541c955cd1abf00ea268
abdul-iman <abdulle.a.iman@gmail.com>
2024-12-04 15:55:05 +0000

    issue #2 fixed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ while (playAgain === false) {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   const userGuess = prompt("Guess a number between 1 and 10:");
 
-  if (userGuess = randomnumber) {
+  if (userGuess === randomnumber) {
     alert("Congratulations! You guessed the correct number.");
     score++;
   } else {
